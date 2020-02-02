17a7b19860df3196d1e8950c1a0a08b7f029880f
Franel <franel@debian>
2020-02-02 23:24:17 +0700

    02/02/2020 23:24

--- linearRegCostFunction.m
+++ linearRegCostFunction.m
@@ -19,16 +19,17 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+J = sum( ((X * theta) - y) .^ 2 ) / (2 * m) + sum(theta(2:end) .^ 2) * lambda / (2 * m);
 
 
 
-
-
-
-
-
-
-
+for i = 1:size(X, 2)
+	if i == 1
+		grad(i) = sum(((X * theta) - y) .* X(:, i)) / m;
+	else
+		grad(i) = (sum(((X * theta) - y) .* X(:, i)) / m) + (lambda * theta(i) / m);
+	end
+end
 
 % =========================================================================
 
